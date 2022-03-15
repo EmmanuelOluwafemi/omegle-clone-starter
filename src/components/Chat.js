6c96096159d4b42d12889b4216daf02d67e33f1f
Emmanuel Yusuf <Emaz4me@gmail.com>
2022-03-15 13:41:48 +0100

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -12,10 +12,6 @@ const Chat = () => {
 
   return (
     <div className='absolute bottom-full right-7 w-72 h-96 bg-gray-900 rounded shadow-xl p-6 pb-0'>
-        
-        <div className='w-full h-5/6 flex flex-col justify-center items-center'>
-            <p className='text-gray-300 text-sm'>There are no messages here.</p>
-        </div>
 
         <form onSubmit={handleSubmit} className='w-full border-t-2 border-gray-700 h-1/6 flex items-center gap-4'>
             <input value={message} onChange={(e) => setMessage(e.target.value)} className='w-full h-full border-none outline-none bg-transparent text-white' placeholder='Write something here' />
