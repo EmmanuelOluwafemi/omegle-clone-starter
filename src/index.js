f2a4f49f64bbb650492d93b2efd87c5c52757798
Emmanuel Yusuf <Emaz4me@gmail.com>
2022-03-15 12:09:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HMSRoomProvider } from '@100mslive/react-sdk';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
+    <HMSRoomProvider>
+    <App />
+  </HMSRoomProvider>,
   document.getElementById('root')
 );
 
